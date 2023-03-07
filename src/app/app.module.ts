24f5c5c2217ca701a403e66ab05a86c140ba738c
Anastasija <anastasija.petrova88@gmail.com>
2023-03-07 16:31:21 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CityComponent } from './city.component';
+import { UiCardComponent } from './ui-card/ui-card.component';
+import {MatButtonModule} from '@angular/material/button';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageComponent } from './page/page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityComponent,
+    UiCardComponent,
+    PageComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/city.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'city',
+    template: '<h2>City</h2>'
+})
+export class CityComponent{
+
+}
